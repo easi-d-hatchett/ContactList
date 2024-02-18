10bddca19f75c94a6f0f6d9cb5638770b77f47e5
Easi-d-Hatchett <easi.hatchett3@gmail.com>
2024-02-18 09:17:39 -0500

    Fetching Data and useEffect

--- vite-project/src/components/ContactList.jsx
+++ vite-project/src/components/ContactList.jsx
@@ -1,7 +1,29 @@
 import React from "react"; 
+import ContactRow from "./ContactRow";
+import { useEffect } from "react";
 
+const dummyContacts = [
+    { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+    { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+    { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+  ];
 
 export default function ContactList() { 
+    const [contacts, setContacts] = useState(dummyContacts);
+    useEffect(()=>{useEffect(() => {
+        async function fetchContacts() {
+          try { const response = await fetch(
+            "https://jsonplace-univclone.herokuapp.com/users"
+          );
+          const result = await response.json();
+          setContacts(result);
+            
+          } catch (error) {
+            console.error(error);
+          }
+        }
+        fetchContacts()
+      }, []);},[])
 
   return ( 
         <table>
@@ -17,8 +39,9 @@ export default function ContactList() {
               <td>Phone</td>
             </tr>
             {
-               // Map over data here
-             }
+            contacts.map((contact)=>{
+                return<ContactRow key={contact.id}contact={contact} />;
+            })}
           </tbody>
         </table>
     ); 
